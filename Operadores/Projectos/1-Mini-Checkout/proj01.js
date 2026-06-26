8f41bc5a0fc80e64f102fbb6bd364281278c64ad
JoaoAntoniodev988 <joaodesousaantonio2@gmail.com>
2026-06-26 14:13:21 +0100

    s

--- Operadores/Projectos/1-Mini-Checkout/proj01.js
+++ Operadores/Projectos/1-Mini-Checkout/proj01.js
@@ -1,7 +1,18 @@
 let valorSubtotal = 120;
 const cupomDesconto = 'QUERO10';
 
-const desconto = (cupomDesconto === 'QUERO10') ? 10 : 0;
-
+const desconto = (cupomDesconto === 'QUERO10') ? 10 : 0; //ternário
 valorSubtotal -= desconto;
 console.log(valorSubtotal)  //110
+
+const ehClientePremium = false;
+
+// O frete é grátis se o valor final for maior ou igual a 100 OU se o cliente for Premium.
+const ganhouFreteGratis = (valorSubtotal >= 100) || ehClientePremium; //comparação e operador lógico
+console.log(ganhouFreteGratis)
+
+// Lembramos que: ganhouFreteGratis = true e valorSubtotal = 110
+const valorFrete = ganhouFreteGratis ? 0 : 15;
+
+const totalPagar = valorSubtotal + valorFrete;
+console.log(totalPagar);
