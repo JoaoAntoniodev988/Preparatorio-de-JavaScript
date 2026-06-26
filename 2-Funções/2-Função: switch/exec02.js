ea37d1e51361111233e17fcb4a1b9716375683da
JoaoAntoniodev988 <joaodesousaantonio2@gmail.com>
2026-06-26 22:40:27 +0100

    s

--- 2-Funções/2-Função: switch/exec02.js
+++ 2-Funções/2-Função: switch/exec02.js
@@ -1,1 +1,15 @@
-const diaSemana = 1;
+const opcaoEscolhida = 2;
+
+switch (opcaoEscolhida) {
+    case 1:
+        console.log("Escolher: Hambúrquer com Batatas");
+        break;
+    case 2:
+        console.log("Escolheu: Pizza de Pepperoni");
+        break;
+    case 3:
+        console.log("Escolheu: Salada Saudável.");
+        break;
+    default:
+        console.log("Opção inválida! Escolha de 1 a 3.");
+}
