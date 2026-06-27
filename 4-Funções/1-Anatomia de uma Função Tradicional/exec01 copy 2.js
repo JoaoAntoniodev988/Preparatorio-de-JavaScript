3426abdccfc27d8800372aa4695206215faa1e49
JoaoAntoniodev988 <joaodesousaantonio2@gmail.com>
2026-06-27 16:07:06 +0100

    s

--- 4-Funções/1-Anatomia de uma Função Tradicional/exec01 copy 2.js
+++ 4-Funções/1-Anatomia de uma Função Tradicional/exec01 copy 2.js
@@ -1,14 +1,18 @@
-function calcularTotalPromocao(listadePrecos) {
+function calcularTotalPromocao(listaDePrecos) {
     let total = 0;
-    for(const preco of listadePrecos) {
+    
+    for (const preco of listaDePrecos) {
         if (preco < 10) {
-            continue;
+            continue; // Salta os menores de 10€!
         }
-
-        total += preco;
+        total = total + preco;
     }
-
-    return total
+    
+    return total; // Devolve o valor final acumulado!
 }
 
-console.log(total)
+// A testar a máquina que criaste:
+const faturaCliente = [5, 20, 3, 50];
+const resultadoFinal = calcularTotalPromocao(faturaCliente);
+
+console.log("O cliente vai pagar: " + resultadoFinal + "€"); // Imprime: 70€
---
+++ 4-Funções/2-Tipos de Funções/exec02.js
@@ -0,0 +1,7 @@
+// FUNÇÃO TRADICIONAL (Para transformares)
+// function calcularIdade(anoNascimento) {
+//     return 2026 - anoNascimento;
+// }
+
+// Transformação
+const calcularIdade = (anoNascimento) => 2026 - anoNascimento
