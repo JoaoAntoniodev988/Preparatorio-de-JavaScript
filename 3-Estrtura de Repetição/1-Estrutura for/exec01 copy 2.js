a5a7f4666b533083c051885fd1f095447689bda8
JoaoAntoniodev988 <joaodesousaantonio2@gmail.com>
2026-06-27 04:03:13 +0100

    s

---
+++ 3-Estrtura de Repetição/1-Estrutura for/exec01 copy 2.js
@@ -0,0 +1,7 @@
+let quatidadeDeErros = 0;
+
+for (let i = 1; i < 3; i++) {
+    quatidadeDeErros = quatidadeDeErros + 1;
+}
+
+console.log("Erros detetados: " + quatidadeDeErros);
---
+++ 3-Estrtura de Repetição/1-Estrutura for/exec01 copy.js
@@ -0,0 +1,7 @@
+let pontocaoTotal = 0;
+
+for (let i = 1; i <= 4; i ++) {
+    pontocaoTotal = pontocaoTotal + 10;
+}
+
+console.log("Pontuação Final: " + pontocaoTotal + " pontos!") 
