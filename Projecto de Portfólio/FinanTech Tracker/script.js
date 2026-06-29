ee484d4d5e040f520e0638b642c2c2067171c399
JoaoAntoniodev988 <joaodesousaantonio2@gmail.com>
2026-06-29 15:59:47 +0100

    u

---
+++ Projecto de Portfólio/FinanTech Tracker/script.js
@@ -0,0 +1,13 @@
+// 1. Selecionando os elementos que vamos precisar manipular
+const form = document.querySelector("#transaction-form");
+const inputdescription = document.querySelector("#description");
+const inputAmount = document.querySelector("#amount");
+const transactionContainer = document.querySelector("#transactionContainer");
+
+// Selecionando os cartões de exibição
+
+const incomeDiplay = document.querySelector("#income-display");
+const expenseDisplay = document.querySelector("#expense-display")
+const totalDisplay = document.querySelector("#total-display")
+
+console.log("Mentor, JavaScript está conectado com sucesso!");
